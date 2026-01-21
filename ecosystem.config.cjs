39ee6844f9851e715adcf7ab344f3ba847a10032
riddhilahare14 <lahare.riddhi14@gmail.com>
2026-01-21 18:36:47 +0530

    changed ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,23 +1,21 @@
 module.exports = {
   apps: [
     {
-      name: "my-app", // Application name
-      script: "./server.js", // Script to start the app
+      name: "tnt-express-app", // Application name
+      script: "./index.js", // Script to start the app
       cwd: "./", // Current working directory (optional)
-      exec_mode: "cluster", // Execution mode: "fork" or "cluster"
-      instances: "max", // Number of instances to run (or a specific number)
+      exec_mode: "fork", // Execution mode: "fork" or "cluster"
+      instances: 1, // Number of instances to run (or a specific number)
       autorestart: true, // Automatically restart the app if it crashes
       watch: false, // Watch for file changes and restart the app (good for development)
       max_memory_restart: "1G", // Restart the app if it exceeds a memory limit
       merge_logs: true, // Merge logs from all instances
-      error_file: "../logs/my-app-error.log", // Error log path
-      out_file: "../logs/my-app-output.log", // Output log path
       time: true, // Add timestamps to logs
 
       env: {
         // Default environment variables
         NODE_ENV: "development",
-        PORT: 3000,
+        PORT: 8000,
       },
       env_production: {
         // Environment variables for the "production" environment
